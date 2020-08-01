a39bf47e1e74a437de109d51db6327a533d8a9f8
Javkhaa0128 <javkhaa0128@gmail.com>
2020-08-01 11:34:34 -0400

    @BI_23 add Login.java

---
+++ src/HappyB20.java
@@ -0,0 +1,18 @@
+public class HappyB20 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println("Javkhlantugs");
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
